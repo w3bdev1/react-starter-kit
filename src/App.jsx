2c03c6661de790e6f774fa16c66c5a42eb30348b
Sudeep Biswas <w3bdev@protonmail.com>
2023-02-15 21:38:41 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+
+const App = () => {
+  return (
+    <h1>Welcome to React Starter Kit</h1>
+  )
+};
+
+const container = document.getElementById("root");
+const root = createRoot(container);
+root.render(React.createElement(App));
